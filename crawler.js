811a9445db8994ecac00c5462ca1370fd6b99422
kqwang123 <32421263+kqwang123@users.noreply.github.com>
2022-04-19 07:44:08 -0500

    Add files via upload

--- crawler.js
+++ crawler.js
@@ -1,43 +1,40 @@
+// node crawler.js to run code
+
 var fs = require('fs');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-var url = "https://en.wikipedia.org/wiki/Philosophy";
+var link = "https://en.wikipedia.org/wiki/Philosophy";
 
 var noIndex = "<meta name=\"robots\" content=\"noindex\"></meta>";
-
-fetchData(url).then( (res) => {
-    const html = res.data;
-    const $ = cheerio.load(html);
-    var title = $("title").text();
-
-    let str = $.text();
-
-    var today = new Date();
-    var timestamp = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() + ' ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var json = {
-        title : title,
-        link : url,
-        contents : str,
-        time : timestamp
-    };
-    writeFile(json);
-});
+fetchData(link);
 
 function writeFile(data) {
-    fs.writeFile ("data.json", JSON.stringify(JSON.parse(data)), function(err) {
+    fs.writeFile ("data.json", JSON.stringify(data), function(err) {
         if (err) throw err;
         }
     );
 }
 
-async function fetchData(urlLink) {
+async function fetchData(url) {
     // make http call to url
-    let response = await axios(urlLink).catch((err) => console.log(err));
+    let response = await axios(url).catch((err) => console.log(err));
 
     if(response.status !== 200){
         console.log("Error occurred while fetching data");
         return;
     }
-    return response;
+    const html = response.data;
+    const $ = cheerio.load(html);
+    var titleVar = $("title").text();
+    let str = $.text();
+    var today = new Date();
+    var timestamp = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() + ' ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var json = {
+        title : titleVar,
+        link : url,
+        contents : str,
+        time : timestamp
+    };
+    writeFile(json);
 }
